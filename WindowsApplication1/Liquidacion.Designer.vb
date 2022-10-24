73e1221b5a8292294ee19fe6bd0e16e9e1eaabf2
Lucas Piputto <lucaspiputto@gmail.com>
2022-10-24 18:02:47 -0300

    Modifica formularios Listas y Liquidacion

--- WindowsApplication1/Liquidacion.Designer.vb
+++ WindowsApplication1/Liquidacion.Designer.vb
@@ -32,58 +32,64 @@ Partial Class Liquidacion
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(92, 43)
+        Me.Label1.Location = New System.Drawing.Point(123, 53)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.Size = New System.Drawing.Size(126, 17)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Nombre del cliente"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(92, 87)
+        Me.Label2.Location = New System.Drawing.Point(123, 107)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(98, 13)
+        Me.Label2.Size = New System.Drawing.Size(131, 17)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Nombre del articulo"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(79, 126)
+        Me.Label3.Location = New System.Drawing.Point(105, 155)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(126, 13)
+        Me.Label3.Size = New System.Drawing.Size(169, 17)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Cantidad y precio unitario" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(101, 164)
+        Me.Label4.Location = New System.Drawing.Point(135, 202)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(76, 13)
+        Me.Label4.Size = New System.Drawing.Size(101, 17)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "Total a abonar"
         '
         'ButtonCalculo
         '
-        Me.ButtonCalculo.Location = New System.Drawing.Point(102, 192)
+        Me.ButtonCalculo.Location = New System.Drawing.Point(136, 236)
+        Me.ButtonCalculo.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ButtonCalculo.Name = "ButtonCalculo"
-        Me.ButtonCalculo.Size = New System.Drawing.Size(85, 46)
+        Me.ButtonCalculo.Size = New System.Drawing.Size(113, 57)
         Me.ButtonCalculo.TabIndex = 9
         Me.ButtonCalculo.Text = "VER LAS LISTAS"
         Me.ButtonCalculo.UseVisualStyleBackColor = True
         '
         'Liquidacion
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(382, 353)
         Me.Controls.Add(Me.ButtonCalculo)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Liquidacion"
         Me.Text = "LIQUIDACION"
         Me.ResumeLayout(False)
--- WindowsApplication1/Listas.Designer.vb
+++ WindowsApplication1/Listas.Designer.vb
@@ -32,56 +32,64 @@ Partial Class Listas
         'ListBoxEfectivo
         '
         Me.ListBoxEfectivo.FormattingEnabled = True
-        Me.ListBoxEfectivo.Location = New System.Drawing.Point(25, 72)
+        Me.ListBoxEfectivo.ItemHeight = 16
+        Me.ListBoxEfectivo.Location = New System.Drawing.Point(37, 101)
+        Me.ListBoxEfectivo.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ListBoxEfectivo.Name = "ListBoxEfectivo"
-        Me.ListBoxEfectivo.Size = New System.Drawing.Size(75, 95)
+        Me.ListBoxEfectivo.Size = New System.Drawing.Size(99, 116)
         Me.ListBoxEfectivo.TabIndex = 0
         '
         'ListBoxCuotas
         '
         Me.ListBoxCuotas.FormattingEnabled = True
-        Me.ListBoxCuotas.Location = New System.Drawing.Point(172, 72)
+        Me.ListBoxCuotas.ItemHeight = 16
+        Me.ListBoxCuotas.Location = New System.Drawing.Point(233, 101)
+        Me.ListBoxCuotas.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ListBoxCuotas.Name = "ListBoxCuotas"
-        Me.ListBoxCuotas.Size = New System.Drawing.Size(75, 95)
+        Me.ListBoxCuotas.Size = New System.Drawing.Size(99, 116)
         Me.ListBoxCuotas.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(22, 33)
+        Me.Label1.Location = New System.Drawing.Point(33, 53)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(92, 13)
+        Me.Label1.Size = New System.Drawing.Size(119, 17)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "PAGO EFECTIVO"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(169, 33)
+        Me.Label2.Location = New System.Drawing.Point(229, 53)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(84, 13)
+        Me.Label2.Size = New System.Drawing.Size(109, 17)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "PAGO CUOTAS"
         '
         'ButtonVolver
         '
-        Me.ButtonVolver.Location = New System.Drawing.Point(98, 195)
+        Me.ButtonVolver.Location = New System.Drawing.Point(135, 252)
+        Me.ButtonVolver.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ButtonVolver.Name = "ButtonVolver"
-        Me.ButtonVolver.Size = New System.Drawing.Size(85, 35)
+        Me.ButtonVolver.Size = New System.Drawing.Size(113, 43)
         Me.ButtonVolver.TabIndex = 6
         Me.ButtonVolver.Text = "VOLVER"
         Me.ButtonVolver.UseVisualStyleBackColor = True
         '
         'Listas
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(382, 353)
         Me.Controls.Add(Me.ButtonVolver)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ListBoxCuotas)
         Me.Controls.Add(Me.ListBoxEfectivo)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Listas"
         Me.Text = "LISTAS"
         Me.ResumeLayout(False)
